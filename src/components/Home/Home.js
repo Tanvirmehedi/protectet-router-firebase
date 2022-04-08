3b7c1e4606cb3f27db4666010f69d3e635ed5d9c
Tanvirmehedi <tjmehedi201@gmail.com>
2022-04-09 02:15:36 +0600

    impliment the proytected route

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase.init";
 
 const Home = () => {
+  const [user] = useAuthState(auth);
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="bg-slate-500 w-4/12 mx-auto my-9 text-slate-50 rounded-lg py-5">
+      <div className="text-xl font-bold">
+        Name Of User : {user ? user.displayName : "No data Found"}
+      </div>
+      <div>
+        <img
+          className="mx-auto rounded-md"
+          src={user ? user.photoURL : "No Data"}
+          alt={user?.displayName}
+        />
+      </div>
+      <div>
+        <h1>Email: {user?.email ? user?.email : "No Data Exist"}</h1>
+      </div>
     </div>
   );
 };
