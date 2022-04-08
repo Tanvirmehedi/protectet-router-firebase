141a7610a1dc0d8e90c62a4cdc218b7afbc6157c
Tanvirmehedi <tjmehedi201@gmail.com>
2022-04-09 02:03:38 +0600

    impliment the proytected route

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -19,15 +19,8 @@ const useFirebase = () => {
         setUser(user);
       })
       .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorMessage);
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
       });
   };
 
